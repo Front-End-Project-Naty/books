926c6c0846f6225a4d4f7ff524bfcf07998cde9c
Naty <natyaith@yahoo.com.br>
2023-05-18 14:54:01 -0300

    atualizando imagens livros

--- src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -1,7 +1,12 @@
-import livro from '../../imagens/livro.png'
+import livro1 from '../../imagens/livro.png'
+import livro2 from '../../imagens/livro2.png'
+import livro3 from '../../imagens/livro3.png'
+import livro4 from '../../imagens/livro4.png'
+
 
 export const livros = [
-    {nome: 'Stranger Cats', id: 1, src:livro},
-    {nome: 'Marley and Me', id: 2, src:livro},
-    {nome: 'Finding Gobi', id: 3, src:livro},
+    {nome: 'Stranger Cats', id: 1, src:livro1},
+    {nome: 'Marley e eu', id: 2, src:livro2},
+    {nome: 'Procurando Gobi', id: 3, src:livro3},
+    {nome: 'Pets', id: 4, src:livro4},
 ]
