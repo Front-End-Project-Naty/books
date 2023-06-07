f52fc3e22b0512d9f9d788fe532c655c127084be
Naty <natyaith@yahoo.com.br>
2023-06-06 22:24:11 -0300

    add pagina favoritos

--- src/rotas/Favoritos.js
+++ src/rotas/Favoritos.js
@@ -1,17 +1,76 @@
-import Pesquisa from '../componentes/Pesquisa'
-import styled from 'styled-components'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import styled from 'styled-components';
+import { getFavoritos } from '../servicos/favoritos';
+import livroImg from '../imagens/livro3.png'
 
 const AppContainer = styled.div`
   width: 100%;
   height: 100vh;
   background-color: #c3e4f5;
 `
+const ResultadoContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`
+
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 0;
+    cursor: pointer;
+    text-align: center;
+    padding: 0 100px;
+    p {
+        width: 200px;
+        color: black;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
+const Titulo = styled.h2`
+    color: black;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
+    padding-top: 35px
+`
 
 function Favoritos() {
+  const [favoritos, setFavoritos] = useState([])
+
+  async function fetchFavoritos() {
+    const favoritosDaAPI = await getFavoritos ()
+    setFavoritos(favoritosDaAPI)
+  }
+
+  useEffect(() => {
+    fetchFavoritos()
+  }, [])
+
   return (
     <AppContainer>
-      <Pesquisa />
-    </AppContainer>
+     <div>
+       <Titulo>Aqui estão seus livros favoritos:</Titulo>
+       <ResultadoContainer>
+         {
+           favoritos.length !== 0 ? favoritos.map(favorito => (
+             <Resultado>
+               <p>{favorito.nome}</p>
+               <img src={livroImg} alt="livro favorito"/>
+             </Resultado>
+           )) : null
+         }
+       </ResultadoContainer>
+     </div>
+   </AppContainer>
   );
 }
 
